a644595181cd195ea973eebad9227c6586e275bf
marcosun <marcoinbristol@gmail.com>
2018-08-15 13:45:54 +0800

    [core] Remove demo and config folders

--- demo/config/webpack.analyse.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const prod = require('./webpack.prod.js');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-module.exports = merge.smart(prod, {
-  plugins: [
-    new BundleAnalyzerPlugin(),
-  ],
-});
